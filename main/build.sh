4da1e562c67a594dd137ffa465a57c1eed99d2b6
FrodoChen <idiottiger@gmail.com>
2013-06-17 17:40:50 +0800

    add new build script.

--- main/build.sh
+++ main/build.sh
@@ -1,2 +1,14 @@
+rm -rf release
+ant clean
 export NDK_PATH=/cygdrive/d/dev/android-bundle/android-ndk-r8e/ndk-build
 $NDK_PATH -C .
+
+ant release
+
+mkdir release
+
+cp -f "bin/classes.jar" "release/libpvrtexture.jar"
+cp -rf "libs/armeabi" "release/armeabi"
+cp -rf "libs/armeabi-v7a" "release/armeabi-v7a"
+
+chmod 666 -R release
